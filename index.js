6479ac2c361f26b4ce30ad185e09b7b51ff6039d
abu bokor suraj <abusurajbd@gmail.com>
2022-11-27 10:28:38 +0600

    admin role api created

--- index.js
+++ index.js
@@ -156,7 +156,16 @@ app.get('/myorders/buyer/:email', async(req,res)=>{
     accessToken:''
   })
 })
-  } finally {
+// isAdmin api
+app.get('/users/admin/:email', async (req, res) => {
+  const email = req.params.email;
+  const query = { email }
+  const user = await usersCollection.findOne(query);
+  res.send({ isAdmin: user?.accountType === 'admin' });
+})
+
+  }
+   finally {
   }
 }
 run().catch((err) => console.log(err));
