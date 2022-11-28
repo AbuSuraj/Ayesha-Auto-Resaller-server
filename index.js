a5f7652149891b037d1da8e7935e8a78d839450f
abu bokor suraj <abusurajbd@gmail.com>
2022-11-28 12:12:19 +0600

    deleteting a product by seller

--- index.js
+++ index.js
@@ -161,6 +161,13 @@ app.get('/myproducts/seller/:email', async(req,res)=>{
   res.send(myproducts)
 
 })
+// deleteting a product by seller 
+app.delete("/product/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: ObjectId(id) };
+  const result = await productCollection.deleteOne(query);
+  res.send(result);
+});
 // my orders get api
 app.get('/myorders/buyer/:email', async(req,res)=>{
    const email = req.params.email;
