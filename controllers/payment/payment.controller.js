c40d4ae51e4f611d087329463005ddabc8509fe5
Abu Suraj <abusurajbd@gmail.com>
2024-03-20 00:30:25 +0600

    add payment converted code

--- controllers/payment/payment.controller.js
+++ controllers/payment/payment.controller.js
@@ -2,44 +2,91 @@ const Payment = require('../../model/payment/Payment.model.js');
 const Booking = require('../../model/payment/Payment.model.js');
 const Product = require('../../model/products/Products.model.js');
 // const { bookingsCollection, productCollection } = require('../your-database-setup');  
-
-async function handlePayment(req, res) {
+exports.addPayment = async (req, res) => {
   try {
     const paymentData = req.body;
+    const newPayment = await Payment.create(paymentData);
 
-    // Insert payment into Payment collection
-    const payment = new Payment(paymentData);
-    const result = await payment.save();
-
-    // Update booking status and product status
+    // Update booking to mark as paid
     const { bookingId, product_id, transactionId } = paymentData;
-    const filter = { _id: ObjectId(bookingId) };
-    const query = { _id: ObjectId(product_id) };
-
-    const updateBookingDoc = {
+    const filter = { _id: bookingId };
+    const bookingUpdate = {
       $set: {
         paid: true,
-        transactionId,
+        transactionId: transactionId,
       },
     };
+    await Booking.updateOne(filter, bookingUpdate);
 
-    const updateProductDoc = {
+    // Update product to mark as paid
+    const productFilter = { _id: product_id };
+    const productUpdate = {
       $set: {
         isPaid: true,
       },
     };
+    await Product.updateOne(productFilter, productUpdate);
 
-    // Update the booking status
-    const updatedBookingResult = await Booking.updateOne(filter, updateBookingDoc);
-
-    // Update the product status
-    const updatedProductResult = await Product.updateOne(query, updateProductDoc);
-
-    res.json(result);
+    res.json(newPayment);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    console.error("Error adding payment:", error);
+    res.status(500).json({ error: "Failed to add payment" });
   }
-}
+};
 
-module.exports = { handlePayment };
+exports.createPaymentIntent = async (req, res) => {
+  try {
+    const { booking } = req.body;
+    const { resalePrice } = booking;
+    const paymentIntent = await stripe.paymentIntents.create({
+      currency: 'bdt',
+      amount: resalePrice * 100, // Amount in cents
+      payment_method_types: ['card'],
+    });
+    res.json({ clientSecret: paymentIntent.client_secret });
+  } catch (error) {
+    console.error("Error creating payment intent:", error);
+    res.status(500).json({ error: "Failed to create payment intent" });
+  }
+};
+
+// async function handlePayment(req, res) {
+//   try {
+//     const paymentData = req.body;
+
+//     // Insert payment into Payment collection
+//     const payment = new Payment(paymentData);
+//     const result = await payment.save();
+
+//     // Update booking status and product status
+//     const { bookingId, product_id, transactionId } = paymentData;
+//     const filter = { _id: ObjectId(bookingId) };
+//     const query = { _id: ObjectId(product_id) };
+
+//     const updateBookingDoc = {
+//       $set: {
+//         paid: true,
+//         transactionId,
+//       },
+//     };
+
+//     const updateProductDoc = {
+//       $set: {
+//         isPaid: true,
+//       },
+//     };
+
+//     // Update the booking status
+//     const updatedBookingResult = await Booking.updateOne(filter, updateBookingDoc);
+
+//     // Update the product status
+//     const updatedProductResult = await Product.updateOne(query, updateProductDoc);
+
+//     res.json(result);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: 'Internal Server Error' });
+//   }
+// }
+
+// module.exports = { handlePayment };
